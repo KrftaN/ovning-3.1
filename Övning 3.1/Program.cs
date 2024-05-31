7140f7cd26ceea5144aa95ad4a3a7ba7286c26c2
KraftaN <63770425+KrftaN@users.noreply.github.com>
2024-05-31 13:44:09 +0200

    Add project files.

---
+++ Övning 3.1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace övning_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange din ålder:");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age >= 16 && age <= 19)
+            {
+                Console.WriteLine("Du får delta i tävlingen.");
+            }
+            else if (age < 16)
+            {
+                Console.WriteLine("Du är för ung för att delta i tävlingen.");
+            }
+            else
+            {
+                Console.WriteLine("Du är för gammal för att delta i tävlingen.");
+            }
+        }
+    }
+}
